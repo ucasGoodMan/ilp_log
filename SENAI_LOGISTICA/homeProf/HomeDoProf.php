4a65a5f767d9236dee06abaeff59da43f18431bc
ucasGoodMan <lucas.k.becker@hotmail.com>
2024-05-21 11:31:32 -0300

    ;;;
    ajeitando algumas telas

--- SENAI_LOGISTICA/homeProf/HomeDoProf.php
+++ SENAI_LOGISTICA/homeProf/HomeDoProf.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <title>SIDEBAR</title>
 
-
 </head>
 
 <body>
@@ -97,21 +96,6 @@
     </section>
     <script src="sidebar.js"></script>
 
-    <form action="process.php" method="post">
-        <div class="forms">
-            <input class="i a" type="text" name="npedido" placeholder="Número de pedidos">
-            <input class="i b" type="text" name="produtos" placeholder="Produtos">
-            <input class="i c" type="text" name="unidade" placeholder="Unidade">
-            <input class="i d" type="text" name="quantidade" placeholder="quantidade">
-            <input class="i e" type="text" name="vlrporunidade" placeholder="valor por unidade">
-            <input class="i f" type="text" name="ncm" placeholder="NCM">
-            <input class="i g" type="text" name="cst" placeholder="CST">
-            <input class="i h" type="text" name="cfop" placeholder="CFOP">
-            <input class="i j" type="text" name="doca" placeholder="DOCA">
-            <input class="i k" type="submit" value="Enviar">
-        
-        </div>
-    </form>
 </body>
 
 </html>
