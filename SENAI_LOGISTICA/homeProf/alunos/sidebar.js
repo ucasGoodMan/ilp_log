c6f7f01d802d1aee4ec1bb6a31afeeea31715d03
Pedro Henrique Pinto <pedro_pinto1@estudante.sesisenai.org.br>
2024-05-22 09:21:11 -0300

    TELA TURMA PRONTA (html,css,js)
    Minha parte ta pronta, é isso. Falta a integração com banco de dados

---
+++ SENAI_LOGISTICA/homeProf/alunos/sidebar.js
@@ -0,0 +1,18 @@
+let arrow = document.querySelectorAll(".arrow")
+for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e) => {
+    let arrowParent = e.target.parentElement.parentElement;
+    arrowParent.classList.toggle("showMenu");
+    });
+}   
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+sidebarBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+});
+console.log(sidebarBtn);
+
+
+
+    
