01ead030997abf99f67189a88d3e9ba428a8fa12
ucasGoodMan <lucas.k.becker@hotmail.com>
2024-07-03 08:11:34 -0300

    mudanças que não mudaram

--- SENAI_LOGISTICA/homeProf/alunos/process.php
+++ SENAI_LOGISTICA/homeProf/alunos/process.php
@@ -1,36 +1,37 @@
 <?php
-	$hostname = "127.0.0.1";
-	$user = "root";
-	$password = "root"; 
-	$database = "senai";
-		
-	$conexao = new mysqli($hostname, $user, $password, $database);
+$hostname = "127.0.0.1";
+$user = "root";
+$password = "root"; 
+$database = "senai";
 
-	if ($conexao -> connect_errno) {
-		echo "Failed to connect to MySQL: " . $conexao -> connect_error;
-		exit();
-	} else {
-		// Evita caracteres especiais (SQL Inject)
-	
-		$nturma = $conexao -> real_escape_string($_POST['nturma']);
-		$nometurma = $conexao -> real_escape_string($_POST['nometurma']);
-        $qntalunos = $conexao -> real_escape_string($_POST['qntalunos']);
+$conexao = new mysqli($hostname, $user, $password, $database);
 
-		
-		$sql = "INSERT INTO `turma`
-			 (`nturma`, `nometurma`, `qntalunos`) 
-			 VALUES 
-			 ('".$nturma."', '".$nometurma."', '".$qntalunos."')";
+if ($conexao->connect_errno) {
+    echo "Failed to connect to MySQL: " . $conexao->connect_error;
+    exit();
+} else {
+    // Verifique se o método de solicitação é POST
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Evita caracteres especiais (SQL Inject)
+        $nturma = $conexao->real_escape_string($_POST['nturma']);
+        $nometurma = $conexao->real_escape_string($_POST['nometurma']);
+        $qntalunos = $conexao->real_escape_string($_POST['qntalunos']);
 
-		   $resultado = $conexao->query($sql);
-		   
-		   $conexao -> close();
-		   header('Location: ../alunos/turmas.php', true, 301);
-		   }
+        // Verifique se os valores não estão vazios
+        if (!empty($nturma) && !empty($nometurma) && !empty($qntalunos)) {
+            $sql = "INSERT INTO `turma` (`nturma`, `nometurma`, `qntalunos`) VALUES ('$nturma', '$nometurma', '$qntalunos')";
+
+            if ($conexao->query($sql) === TRUE) {
+                echo "Novo registro criado com sucesso";
+                header('Location: ../alunos/turmas.php', true, 301);
+                exit();
+            } else {
+                echo "Erro: " . $sql . "<br>" . $conexao->error;
+            }
+        } else {
+            echo "Por favor, preencha todos os campos.";
+        }
+    }
+    $conexao->close();
+}
 ?>
-
-
-
-
-
-
