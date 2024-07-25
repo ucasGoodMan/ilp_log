0f4a8883bc3ed4a584b5457ded040cf72f12c554
Pedro Henrique Pinto <137089049+PedroPnt@users.noreply.github.com>
2024-07-25 03:01:32 -0300

    12

--- SENAI_LOGISTICA/home/recebimento/Container/recebimento_container.php
+++ SENAI_LOGISTICA/home/recebimento/Container/recebimento_container.php
@@ -13,11 +13,21 @@
         }
 
         .container {
-            justify-content: center;    
-            max-width: 100%;
-            max-height: 100%;
-            padding: 20px;
             display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            width: 100%;
+            padding: 20px;
+            background-color: #d6d3d1;
+        }
+
+        form {
+            display: flex;
+            justify-content: center;
+            gap: 50px;
+            flex-wrap: wrap;
+            max-width: 100%;
         }
 
         .form-column {
@@ -25,9 +35,8 @@
             padding: 20px;
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-
-        .bloco {
+            box-sizing: border-box;
+            width: 300px;
             margin-bottom: 20px;
         }
 
@@ -39,7 +48,7 @@
 
         input[type="text"],
         input[type="number"] {
-            width: calc(100% - 20px);
+            width: 100%;
             padding: 10px;
             margin-top: 5px;
             border: 1px solid #ddd;
@@ -55,51 +64,43 @@
             margin: 5px 0;
         }
 
+        input[type="checkbox"] {
+            margin-right: 5px;
+        }
+
         input[type="submit"] {
-            display: block;
-            width: 100%;
+            display: flex;
+            justify-content: center;
+            width: 30%;
             padding: 10px;
-            background-color: #4CAF50;
+            background-color: rgb(37, 91, 168);
             color: white;
             border: none;
             cursor: pointer;
             transition: background-color 0.3s ease;
-            margin-top: 20px;
             border-radius: 4px;
+            margin-top: 20px;
         }
 
         input[type="submit"]:hover {
-            background-color: #45a049;
+            background-color: rgb(37, 91, 130);
         }
     </style>
 </head>
 <body>
 <div class="container">
-    <form method="post" action="process.php" id="form1" name="form1">
-
+    <form method="post" action="process.php" id="mainForm" name="mainForm">
         <div class="form-column">
-            <div class="bloco">
-                <label>Placa do caminhão:</label>
-                <input type="text" name="placa" id="placa" size="20">
-            </div>
-
-            <div class="bloco">
-                <label>Nome do Motorista:</label>
-                <input type="text" name="nomemotori" id="nomemotori" size="20">
-            </div>
-
-            <div class="bloco">
-                <label>Container:</label>
-                <input type="text" name="container" id="container" size="20">
-            </div>
-
-            <div class="bloco">
-                <label>Navio:</label>
-                <input type="text" name="navio" id="navio" size="20">
-            </div>
-
+            <label>Placa do caminhão:</label>
+            <input type="text" name="placa" id="placa" size="20">
+            <label>Nome do Motorista:</label>
+            <input type="text" name="nomemotori" id="nomemotori" size="20">
+            <label>Container:</label>
+            <input type="text" name="container" id="container" size="20">
+            <label>Navio:</label>
+            <input type="text" name="navio" id="navio" size="20">
+            <label>Checklist:</label>
             <div class="checklist">
-                <label>Checklist:</label>
                 <input type="checkbox" name="ContainerDesgastado" value="0"> Container Bem Desgastado <br>
                 <input type="checkbox" name="AvariaLateralDireita" value="0"> Avaria Lateral Direita <br>
                 <input type="checkbox" name="AvariaLateralEsquerda" value="0"> Avaria Lateral Esquerda <br>
@@ -109,28 +110,16 @@
         </div>
 
         <div class="form-column">
-            <div class="bloco">
-                <label>Cliente:</label>
-                <input type="text" name="cliente" id="cliente" size="20">
-            </div>
-
-            <div class="bloco">
-                <label>Tipo:</label>
-                <input type="text" name="tipo" id="tipo" size="20">
-            </div>
-
-            <div class="bloco">
-                <label>Lacre:</label>
-                <input type="text" name="lacre" id="lacre" size="20">
-            </div>
-
-            <div class="bloco">
-                <label>IMO:</label>
-                <input type="number" name="imo" id="imo" size="20">
-            </div>
-
+            <label>Cliente:</label>
+            <input type="text" name="cliente" id="cliente" size="20">
+            <label>Tipo:</label>
+            <input type="text" name="tipo" id="tipo" size="20">
+            <label>Lacre:</label>
+            <input type="text" name="lacre" id="lacre" size="20">
+            <label>IMO:</label>
+            <input type="number" name="imo" id="imo" size="20">
+            <label>Checklist:</label>
             <div class="checklist">
-                <label>Checklist:</label>
                 <input type="checkbox" name="SemLacre" value="0"> Sem Lacre <br>
                 <input type="checkbox" name="AdesivoAvariado" value="0"> Adesivos Avariados <br>
                 <input type="checkbox" name="ExcessoAltura" value="0"> Excesso de Altura <br>
@@ -140,36 +129,23 @@
         </div>
 
         <div class="form-column">
-            <div class="bloco">
-                <label>Lacre SIF:</label>
-                <input type="number" name="lacresif" id="lacresif" size="20">
-            </div>
-
-            <div class="bloco">
-                <label>Nº ONU:</label>
-                <input type="number" name="onu" id="onu" size="20">
-            </div>
-
-            <div class="bloco">
-                <label>Temperatura:</label>
-                <input type="number" name="temp" id="temp" size="20">
-            </div>
-
+            <label>Lacre SIF:</label>
+            <input type="number" name="lacresif" id="lacresif" size="20">
+            <label>Nº ONU:</label>
+            <input type="number" name="onu" id="onu" size="20">
+            <label>Temperatura:</label>
+            <input type="number" name="temp" id="temp" size="20">
+            <label>Checklist:</label>
             <div class="checklist">
-                <label>Checklist:</label>
                 <input type="checkbox" name="ExcessoFrontal" value="0"> Excesso Frontal <br>
                 <input type="checkbox" name="PainelAvariado" value="0"> Painel Avariado <br>
                 <input type="checkbox" name="SemCaboEnergia" value="0"> Sem Cabo Energia <br>
                 <input type="checkbox" name="SemLona" value="0"> Sem Lona <br>
             </div>
         </div>
+        
+        <input type="submit" value="Submit">
     </form>
-
-    <div style="max-width: 1200px; margin: 20px auto; padding: 20px;">
-    <input type="submit" value="Cadastrar">
 </div>
-</div>
-
-
 </body>
 </html>
