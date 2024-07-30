83d78c5322be1a52da8dd7cb4a54e14deb2d42c8
ucasGoodMan <143806635+ucasGoodMan@users.noreply.github.com>
2024-07-30 20:47:56 -0300

    tela de meus pedidos

---
+++ SENAI_LOGISTICA/homeProf/itens/buscarpedido.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../home/movimentacao/docar.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <title>Detalhes do Pedido</title>
+</head>
+
+<body>
+    <a class="back-button" onclick="window.history.back();"><i class='bx bx-log-out'></i> Voltar</a>
+    <div class="container">
+        <h1>Detalhes do Pedido</h1>
+        <ul class="produto-list">
+            <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "root";
+            $dbname = "senai";
+
+            // Cria a conexão
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            // Verifica a conexão
+            if ($conn->connect_error) {
+                die("Falha na conexão: " . $conn->connect_error);
+            }
+
+            // Obtém o pedido_id da URL e verifica se está definido
+            if (isset($_GET['pedido_id'])) {
+                $pedido_id = $_GET['pedido_id'];
+
+                // Consulta SQL para obter os produtos do pedido
+                $sql_produtos = "SELECT * FROM produtos WHERE pedido_id = '$pedido_id'";
+                $result_produtos = $conn->query($sql_produtos);
+
+                // Verifica se há resultados
+                if ($result_produtos->num_rows > 0) {
+                    // Loop através dos produtos do pedido
+                    while ($row_produtos = $result_produtos->fetch_assoc()) {
+                        echo "<li class='produto-item'>";
+                        echo "  <ul class='produto-info'>";
+                        echo "    <li class='produto-cod'>Código: " . $row_produtos["cod_prod"] . "</li>";
+                        echo "    <li class='produto-nome'>Produto: " . $row_produtos["nome_produto"] . "</li>";
+                        echo "    <li class='produto-un'>Unidade: " . $row_produtos["un_prod"] . "</li>";
+                        echo "    <li class='produto-quantidade'>Quantidade: " . $row_produtos["qtd_prod"] . "</li>";
+                        echo "    <li class='produto-rsunit'>Preço Unitário: " . $row_produtos["rsunit_prod"] . "</li>";
+                        echo "    <li class='produto-ncm'>NCM: " . $row_produtos["ncm_prod"] . "</li>";
+                        echo "    <li class='produto-cst'>CST: " . $row_produtos["cst_prod"] . "</li>";
+                        echo "    <li class='produto-cfop'>CFOP: " . $row_produtos["cfop_prod"] . "</li>";
+                        echo "  </ul>"; 
+                        echo "</li>"; 
+                    }
+                } else {
+                    echo "<li>Nenhum produto encontrado para este pedido.</li>";
+                }
+            } else {
+                echo "<li>Erro: Pedido ID não fornecido.</li>";
+            }
+
+            // Fecha a conexão
+            $conn->close();
+            ?>
+        </ul>
+    </div>
+    <script src="sidebar.js"></script>
+</body>
+
+</html>
---
+++ SENAI_LOGISTICA/homeProf/itens/meuspedidos.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../home/movimentacao/docar.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <title>Pedidos Doca</title>
+</head>
+<body>
+<a class="back-button" onclick="window.history.back();"><i class='bx bx-log-out'></i> Voltar</a>
+<div class="container">
+    <h1>Lista de Pedidos</h1>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "senai";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Falha na conexão: " . $conn->connect_error);
+    } 
+
+    $sql_pedidos = "SELECT DISTINCT pedidos.id 
+                    FROM pedidos 
+                    JOIN produtos ON pedidos.id = produtos.pedido_id";
+    $result_pedidos = $conn->query($sql_pedidos);
+
+    if ($result_pedidos->num_rows > 0) {
+        echo "<ul class='pedido-list'>";
+        while($row_pedidos = $result_pedidos->fetch_assoc()) {
+            $pedido_id = $row_pedidos["id"];
+            echo "<li class='pedido-item'>";
+            echo "<div class='pedido-info'>";
+            echo "<span class='pedido-idpedido'>Número do Pedido: " . $pedido_id . "</span>";
+            echo "</div>";
+            echo "<form action='buscarpedido.php' method='GET'>";
+            echo "<input type='hidden' name='pedido_id' value='" . $pedido_id . "'>";
+            echo "<input class='pedido-submit' type='submit' value='ABRIR'>";
+            echo "</form>";
+            echo "</li>";
+        }
+        echo "</ul>";
+    } else {
+        echo "Nenhum pedido encontrado.";
+    }
+
+    // Fecha a conexão
+    $conn->close();
+    ?>
+</div>
+<script src="sidebar.js"></script>
+</body>
+</html>
