59eaf7945158bccb3027ce2b308597d223f102ad
ucasGoodMan <lucas.k.becker@hotmail.com>
2024-08-01 09:10:40 -0300

    so ajeitando uns nome

--- SENAI_LOGISTICA/homeProf/index.php
+++ SENAI_LOGISTICA/homeProf/index.php
@@ -7,8 +7,7 @@
     <link rel="stylesheet" href="styleProf.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    <title>HOME PROF</title>
-
+    <title>Home Docente</title>
  
 </head>
 
@@ -92,8 +91,8 @@
     </div>
     <section class="home-section">
         <i class='bx bx-menu'></i>
-        <span class="text">Home do prof</span>
-        <span class="prof">Bem vindo, Professor(a)</span>
+        <span class="text">Home do Docente</span>
+        <span class="prof">Bem vindo, Docente</span>
         <img class="imgProf" src="../imagens/Classroom-cuate.svg" alt="Professor img">
     </section>
     <script src="sidebar.js"></script>
