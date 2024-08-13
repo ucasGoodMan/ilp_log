51e10631ef8f10fbfd27817f5d024042e2a22db0
isacfrancisco <isac0408@gmail.com>
2024-08-13 11:15:33 -0300

    novo banco

--- database/novo senai/senai.sql
+++ database/novo senai/senai.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 13-Ago-2024 às 13:27
+-- Generation Time: 13-Ago-2024 às 14:14
 -- Versão do servidor: 5.7.11
 -- PHP Version: 7.0.3
 
@@ -77,18 +77,21 @@ CREATE TABLE `doca_pedidos` (
   `pedido_id` int(11) DEFAULT NULL,
   `numero_doca` int(11) DEFAULT NULL,
   `data_hora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `nome_produto` varchar(255) NOT NULL
+  `nome_produto` varchar(255) NOT NULL,
+  `status` varchar(65) NOT NULL DEFAULT 'Pendente'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `doca_pedidos`
 --
 
-INSERT INTO `doca_pedidos` (`id`, `pedido_id`, `numero_doca`, `data_hora`, `nome_produto`) VALUES
-(1, 11, 2, '2024-08-13 12:24:42', ''),
-(2, 11, 2, '2024-08-13 12:24:42', ''),
-(3, 11, 33, '2024-08-13 12:32:14', 'core i5'),
-(4, 11, 33, '2024-08-13 12:32:14', 'core i6');
+INSERT INTO `doca_pedidos` (`id`, `pedido_id`, `numero_doca`, `data_hora`, `nome_produto`, `status`) VALUES
+(1, 11, 2, '2024-08-13 12:24:42', '', 'Pendente'),
+(2, 11, 2, '2024-08-13 12:24:42', '', 'Pendente'),
+(3, 11, 33, '2024-08-13 12:32:14', 'core i5', 'ConcluÃ­do'),
+(4, 11, 33, '2024-08-13 12:32:14', 'core i6', 'Concluido'),
+(5, 1, 111, '2024-08-13 13:32:39', 'tesoura', 'Pendente'),
+(6, 1, 23, '2024-08-13 13:55:44', 'tesoura', 'Concluido');
 
 -- --------------------------------------------------------
 
@@ -601,7 +604,7 @@ ALTER TABLE `detalhes_danfe`
 -- AUTO_INCREMENT for table `doca_pedidos`
 --
 ALTER TABLE `doca_pedidos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `estoque`
 --
