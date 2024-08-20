ce069005125c27de1e3ab82529495de2efe8a8a6
Isabella Dias <137089049+PedroPnt@users.noreply.github.com>
2024-08-20 11:30:53 -0300

    .

--- SENAI_LOGISTICA/sidebarALU.php
+++ SENAI_LOGISTICA/sidebarALU.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    <title>Document</title>
+    <title>Document 1</title>
 </head>
 
 <body>
